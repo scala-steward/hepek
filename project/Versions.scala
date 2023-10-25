4aad31d0d44beb643f6db530826bd3787ba83151
Scala Steward <scala_steward@virtuslab.com>
2023-10-25 03:54:19 +0000

    Update tupson to 0.8.0

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,9 +1,9 @@
 object V {
   val scalaTags = "0.11.1"
-  val tupson    = "0.7.0"
+  val tupson    = "0.8.0"
 
   val hepekCore     = "0.2.0"
-  val flexmark    = "0.64.8"
+  val flexmark      = "0.64.8"
   val jsoup         = "1.15.4"
   val openHtmlToPdf = "1.0.2"
 
